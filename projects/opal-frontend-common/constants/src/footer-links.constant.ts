f8237161bec138d2996cc538bb33aba35dfcdae0
Frankie Moran <frankie.jack.moran@gmail.com>
2025-04-08 15:44:20 +0100

    Introduce constants for header/footer links/information so these can be updated across the micro frontends

---
+++ projects/opal-frontend-common/constants/src/footer-links.constant.ts
@@ -0,0 +1,8 @@
+import { IGovukFooterLinks } from '@hmcts/opal-frontend-common/components/govuk/govuk-footer/interfaces';
+
+export const FOOTER_LINKS: IGovukFooterLinks = {
+  licenseLink: 'https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/',
+  licenseVersion: '3.0',
+  copyrightLink:
+    'https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/',
+};
