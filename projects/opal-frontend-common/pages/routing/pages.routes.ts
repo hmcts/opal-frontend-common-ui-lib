30287e3f4670a64d4b8285bd7d11c2b3f5c9ed54
Frankie Moran <frankie.jack.moran@gmail.com>
2025-04-11 14:51:50 +0100

    Resolvers refactored to have their own entry point

--- projects/opal-frontend-common/pages/routing/pages.routes.ts
+++ projects/opal-frontend-common/pages/routing/pages.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { PAGES_ROUTING_TITLES, PAGES_ROUTING_PATHS } from '@hmcts/opal-frontend-common/pages/routing/constants';
 import { authGuard } from '@hmcts/opal-frontend-common/guards/auth';
 import { signedInGuard } from '@hmcts/opal-frontend-common/guards/signed-in';
-import { TitleResolver, userStateResolver } from '@hmcts/opal-frontend-common/resolvers';
+import { TitleResolver } from '@hmcts/opal-frontend-common/resolvers/title';
+import { userStateResolver } from '@hmcts/opal-frontend-common/resolvers/user-state';
 
 export const routing: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
