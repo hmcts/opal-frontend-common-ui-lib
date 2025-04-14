5df35c5470880da36afd31835a15d147f69b7762
Frankie Moran <frankie.jack.moran@gmail.com>
2025-04-14 17:10:43 +0100

    Changes from PO-1289

--- projects/opal-frontend-common/guards/auth/auth.guard.ts
+++ projects/opal-frontend-common/guards/auth/auth.guard.ts
@@ -3,6 +3,9 @@ import { inject } from '@angular/core';
 import { map, catchError, of } from 'rxjs';
 import { PAGES_ROUTING_PATHS } from '@hmcts/opal-frontend-common/pages/routing/constants';
 import { AuthService } from '@hmcts/opal-frontend-common/services/auth-service';
+import { GlobalStore } from '@hmcts/opal-frontend-common/stores/global';
+import { GlobalStoreType } from '@hmcts/opal-frontend-common/stores/global/types';
+import { SSO_ENDPOINTS } from '@hmcts/opal-frontend-common/services/auth-service/constants';
 
 /**
  * A guard that checks if the user is authenticated before allowing access to a route.
@@ -10,6 +13,7 @@ import { AuthService } from '@hmcts/opal-frontend-common/services/auth-service';
  */
 export const authGuard: CanActivateFn = () => {
   const authService: AuthService = inject(AuthService);
+  const globalStore: GlobalStoreType = inject(GlobalStore);
   const router = inject(Router);
 
   return authService.checkAuthenticated().pipe(
@@ -17,7 +21,11 @@ export const authGuard: CanActivateFn = () => {
       return resp;
     }),
     catchError(() => {
-      router.navigate([PAGES_ROUTING_PATHS.children.signIn]);
+      if (globalStore.ssoEnabled()) {
+        window.location.href = SSO_ENDPOINTS.login;
+      } else {
+        router.navigate([PAGES_ROUTING_PATHS.children.signIn]);
+      }
       return of(false);
     }),
   );
