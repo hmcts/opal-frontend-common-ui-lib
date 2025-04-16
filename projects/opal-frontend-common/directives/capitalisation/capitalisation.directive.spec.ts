35d3f8a27377532de7bbdf961da901e0fb1e7812
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2025-04-16 16:32:23 +0100

    Added Capitalisation Directive + Unit tests

---
+++ projects/opal-frontend-common/directives/capitalisation/capitalisation.directive.spec.ts
@@ -0,0 +1,62 @@
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { CapitalisationDirective } from './capitalisation.directive';
+import { UtilsService } from '@hmcts/opal-frontend-common/services/utils-service';
+
+@Component({
+  template: `<div capitaliseAllCharacters><input type="text" /></div>`,
+  imports: [CapitalisationDirective],
+})
+class TestComponent {}
+
+describe('CapitalisationDirective', () => {
+  let inputEl: DebugElement;
+  let mockUtilsService: jasmine.SpyObj<UtilsService>;
+  let fixture: ComponentFixture<TestComponent> | null;
+
+  beforeEach(async () => {
+    mockUtilsService = jasmine.createSpyObj('UtilsService', ['upperCaseAllLetters']);
+    mockUtilsService.upperCaseAllLetters.and.callFake((value: string) => value.toUpperCase());
+
+    TestBed.configureTestingModule({
+      imports: [TestComponent],
+      providers: [{ provide: UtilsService, useValue: mockUtilsService }],
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    inputEl = fixture.debugElement.query(By.css('input'));
+    fixture.detectChanges();
+  });
+
+  afterAll(() => {
+    fixture = null;
+    TestBed.resetTestingModule();
+  });
+
+  it('should create', () => {
+    expect(fixture).toBeTruthy();
+  });
+
+  it('should capitalize input value in real-time', () => {
+    const inputElement = inputEl.nativeElement as HTMLInputElement;
+    inputElement.value = 'test';
+    inputEl.triggerEventHandler('input', { target: inputElement });
+
+    expect(mockUtilsService.upperCaseAllLetters).toHaveBeenCalledWith('test');
+    expect(inputElement.value).toBe('TEST');
+  });
+
+  it('should remove the event listener on destroy', () => {
+    const directiveInstance = inputEl.injector.get(CapitalisationDirective);
+    const removeListenerSpy = jasmine.createSpy('removeListener');
+
+    directiveInstance['removeListener'] = removeListenerSpy;
+
+    directiveInstance.ngOnDestroy();
+
+    expect(removeListenerSpy).toHaveBeenCalled();
+  });
+});
---
+++ projects/opal-frontend-common/directives/capitalisation/capitalisation.directive.ts
@@ -0,0 +1,67 @@
+import { Directive, ElementRef, Renderer2, inject, AfterViewInit, OnDestroy } from '@angular/core';
+import { UtilsService } from '@hmcts/opal-frontend-common/services/utils-service';
+
+@Directive({
+  selector: '[capitaliseAllCharacters]',
+})
+export class CapitalisationDirective implements AfterViewInit, OnDestroy {
+  private inputElement!: HTMLInputElement | null;
+  private readonly el = inject(ElementRef);
+  private readonly renderer = inject(Renderer2);
+  private readonly utilsService = inject(UtilsService);
+
+  /**
+   * A callback function that removes an event listener when invoked.
+   * If no listener is currently attached, this will be `null`.
+   */
+  private removeListener: (() => void) | null = null;
+
+  /**
+   * Lifecycle hook that is called after Angular has fully initialized a component's view.
+   * This method sets up an event listener on an input element within the directive's host element.
+   * The listener capitalizes the input value in real-time as the user types.
+   *
+   * - It first queries for an `input` element within the host element.
+   * - If the input element is found, it attaches an `input` event listener.
+   * - On each input event, the listener:
+   *   - Retrieves the current value of the input field.
+   *   - Capitalizes the value using the `capitalise` method.
+   *   - Updates the input field's value with the capitalized text.
+   *
+   * This ensures that the input value is always displayed in a capitalized format.
+   */
+  ngAfterViewInit(): void {
+    this.inputElement = this.el.nativeElement.querySelector('input');
+
+    if (this.inputElement) {
+      this.removeListener = this.renderer.listen(this.inputElement, 'input', (event: Event) => {
+        const target = event.target as HTMLInputElement;
+        const capitalized = this.capitalise(target.value);
+
+        this.renderer.setProperty(this.inputElement, 'value', capitalized);
+      });
+    }
+  }
+
+  /**
+   * Lifecycle hook that is called when the directive is destroyed.
+   * Cleans up any resources or event listeners to prevent memory leaks.
+   * If a listener removal function is defined, it is invoked and set to null.
+   */
+  ngOnDestroy(): void {
+    if (this.removeListener) {
+      this.removeListener();
+      this.removeListener = null;
+    }
+  }
+
+  /**
+   * Converts the given string to uppercase using the utility service.
+   *
+   * @param value - The string to be capitalised.
+   * @returns The capitalised string with all letters in uppercase.
+   */
+  private capitalise(value: string): string {
+    return this.utilsService.upperCaseAllLetters(value);
+  }
+}
