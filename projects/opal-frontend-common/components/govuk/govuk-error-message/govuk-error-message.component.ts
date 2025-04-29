482dbd2e96d175c073d7f8116401189ed68d7eed
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2025-04-29 16:24:53 +0100

    Minor changes

--- projects/opal-frontend-common/components/govuk/govuk-error-message/govuk-error-message.component.ts
+++ projects/opal-frontend-common/components/govuk/govuk-error-message/govuk-error-message.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'opal-lib-gov-uk-error-message',
+  selector: 'opal-lib-govuk-error-message',
   imports: [],
   templateUrl: './govuk-error-message.component.html',
 })
-export class GovUkErrorMessageComponent {
+export class GovukErrorMessageComponent {
   @Input({ required: true }) error!: boolean;
   @Input({ required: true }) errorMessage!: string;
   @Input({ required: true }) elementId!: string;
