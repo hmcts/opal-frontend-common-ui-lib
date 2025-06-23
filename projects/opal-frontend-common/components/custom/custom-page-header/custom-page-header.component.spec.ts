4ec5efa054521e0fbdaee4136ce1426cf14d9708
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2025-06-23 15:15:35 +0100

    Custom-page-header

---
+++ projects/opal-frontend-common/components/custom/custom-page-header/custom-page-header.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustomPageHeaderComponent } from './custom-page-header.component';
+
+describe('CustomPageHeaderComponent', () => {
+  let component: CustomPageHeaderComponent;
+  let fixture: ComponentFixture<CustomPageHeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CustomPageHeaderComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CustomPageHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/opal-frontend-common/components/custom/custom-page-header/custom-page-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'opal-lib-custom-page-header',
+  imports: [],
+  templateUrl: './custom-page-header.component.html',
+  styleUrl: './custom-page-header.component.css'
+})
+export class CustomPageHeaderComponent {
+
+}
