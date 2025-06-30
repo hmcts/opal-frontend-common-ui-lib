7b6315352dfdbfcdb140c49d7e5b9cc27e341ab9
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2025-06-30 14:44:47 +0100

    Removing the section header component and adding _custom-section-header classes.

--- projects/opal-frontend-common/components/custom/custom-section-header/custom-section-header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-
-@Component({
-  selector: 'opal-lib-custom-section-header, [opal-lib-custom-section-header]',
-  templateUrl: './custom-section-header.component.html',
-  styleUrl: './custom-section-header.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class CustomSectionHeaderComponent {}
