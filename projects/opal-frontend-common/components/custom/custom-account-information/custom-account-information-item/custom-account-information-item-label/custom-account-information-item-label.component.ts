e58a14a0b7e03beb97424802dbabed3e6889352a
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2025-07-01 11:06:50 +0100

    Label and Value Fix

--- projects/opal-frontend-common/components/custom/custom-account-information/custom-account-information-item/custom-account-information-item-label/custom-account-information-item-label.component.ts
+++ projects/opal-frontend-common/components/custom/custom-account-information/custom-account-information-item/custom-account-information-item-label/custom-account-information-item-label.component.ts
@@ -3,8 +3,9 @@ import { Component, ChangeDetectionStrategy, HostBinding } from '@angular/core';
 @Component({
   selector: 'opal-lib-custom-account-information-item-label, [opal-lib-custom-account-information-item-label]',
   templateUrl: './custom-account-information-item-label.component.html',
+  styleUrl: './custom-account-information-item-label.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CustomAccountInformationItemLabelComponent {
-  @HostBinding('class') hostClass = 'govuk-heading-s govuk-!-margin-0';
+  @HostBinding('class') hostClass = 'govuk-body govuk-!-font-size-16 govuk-!-font-weight-bold govuk-!-margin-0';
 }
--- projects/opal-frontend-common/components/custom/custom-account-information/custom-account-information-item/custom-account-information-item-value/custom-account-information-item-value.component.ts
+++ projects/opal-frontend-common/components/custom/custom-account-information/custom-account-information-item/custom-account-information-item-value/custom-account-information-item-value.component.ts
@@ -3,9 +3,8 @@ import { Component, ChangeDetectionStrategy, HostBinding } from '@angular/core';
 @Component({
   selector: 'opal-lib-custom-account-information-item-value, [opal-lib-custom-account-information-item-value]',
   templateUrl: './custom-account-information-item-value.component.html',
-  styleUrl: './custom-account-information-item-value.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CustomAccountInformationItemValueComponent {
-  @HostBinding('class') hostClass = 'govuk-body-s govuk-!-margin-0';
+  @HostBinding('class') hostClass = 'govuk-body govuk-!-font-size-16 govuk-!-margin-0';
 }
