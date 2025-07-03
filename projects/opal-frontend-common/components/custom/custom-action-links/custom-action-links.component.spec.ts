ea08f4717b77b7734073c833ec7d0d92abae35fa
Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>
2025-07-03 16:22:21 +0100

    Initial component architecture

---
+++ projects/opal-frontend-common/components/custom/custom-action-links/custom-action-links.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustomActionLinksComponent } from './custom-action-links.component';
+
+describe('CustomActionLinksComponent', () => {
+  let component: CustomActionLinksComponent;
+  let fixture: ComponentFixture<CustomActionLinksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CustomActionLinksComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CustomActionLinksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
