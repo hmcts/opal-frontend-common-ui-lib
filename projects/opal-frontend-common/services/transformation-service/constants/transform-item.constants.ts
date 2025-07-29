f586276cf36bf6863298f7f1ae93473992e88b65
louisbriggs <louis.briggs@gmail.com>
2025-07-29 10:21:44 +0100

    fix linting issue

--- projects/opal-frontend-common/services/transformation-service/constants/transform-item.constants.ts
+++ projects/opal-frontend-common/services/transformation-service/constants/transform-item.constants.ts
@@ -1,4 +1,4 @@
-import { ITransformItem } from "@hmcts/opal-frontend-common/services/transformation-service/interfaces";
+import { ITransformItem } from '@hmcts/opal-frontend-common/services/transformation-service/interfaces';
 
 export const TRANSFORM_ITEM_DEFAULTS: Pick<ITransformItem, 'dateConfig' | 'timeConfig'> = {
   dateConfig: null,
