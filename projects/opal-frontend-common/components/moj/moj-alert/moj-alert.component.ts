022b5582527eaaf416c2da864bd0fdcce8c52085
louisbriggs <louis.briggs@gmail.com>
2025-08-28 09:46:49 +0100

    Adding dismissed event emitter to alert component

--- projects/opal-frontend-common/components/moj/moj-alert/moj-alert.component.ts
+++ projects/opal-frontend-common/components/moj/moj-alert/moj-alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, Output } from '@angular/core';
 import { MojAlertType } from './constants/alert-types.constant';
 import { CommonModule } from '@angular/common';
 import { MojAlertDismissComponent } from './moj-alert-dismiss/moj-alert-dismiss.component';
@@ -12,6 +12,7 @@ export class MojAlertComponent {
   @Input({ required: true }) ariaLabel!: string;
   @Input({ required: true }) type: MojAlertType = 'information';
   @Input({ required: false }) showDismiss!: boolean;
+  @Output() dismissed = new EventEmitter<void>();
 
   public isVisible: boolean = true;
 
@@ -28,10 +29,12 @@ export class MojAlertComponent {
   /**
    * Dismisses the alert component.
    *
-   * This method sets the component's visibility state to false,
+   * This method emits the dismissed event to it's parent and 
+   * sets the component's visibility state to false,
    * effectively hiding the alert from view.
    */
   public dismiss(): void {
     this.isVisible = false;
+    this.dismissed.emit();
   }
 }
