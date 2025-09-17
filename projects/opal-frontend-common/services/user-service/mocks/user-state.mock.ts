e47111190690861b862fc68ab3bdec0231b97fdf
Frankie Moran <frankie.jack.moran@gmail.com>
2025-09-17 09:07:40 +0100

    Update user state mock for frontend tests

--- projects/opal-frontend-common/services/user-service/mocks/user-state.mock.ts
+++ projects/opal-frontend-common/services/user-service/mocks/user-state.mock.ts
@@ -4,7 +4,7 @@ import { USER_BUSINESS_UNIT_USER_MOCK } from './user-business-unit-user.mock';
 export const USER_STATE_MOCK: IUserState = {
   user_id: 50000000,
   username: 'timmyTest@HMCTS.NET',
-  name: 'timmyTest',
+  name: 'Timmy Test',
   status: 'active',
   version: 1,
   business_unit_users: USER_BUSINESS_UNIT_USER_MOCK,
