c7a021e901de45900f59f7733e433060fd9a5d10
Frankie Moran <frankie.jack.moran@gmail.com>
2025-10-15 14:51:10 +0100

    Linting

--- projects/opal-frontend-common/guards/account/account.guard.ts
+++ projects/opal-frontend-common/guards/account/account.guard.ts
@@ -6,13 +6,13 @@ import { PAGES_ROUTING_PATHS } from '@hmcts/opal-frontend-common/pages/routing/c
 
 /**
  * A route guard that determines whether a user can activate a route based on their account status.
- * 
+ *
  * This guard uses the `OpalUserService` to fetch the logged-in user's state and decides the navigation
  * behavior based on the user's account status:
  * - If the user's status is `active`, the route is activated.
  * - If the user's status is `created`, the user is redirected to the "Account Created" page.
  * - For any other status or in case of an error, the user is redirected to the "Account Created" page.
- * 
+ *
  * @returns An observable that emits either `true` to activate the route or a `UrlTree` to redirect the user.
  */
 export const accountGuard: CanActivateFn = () => {
