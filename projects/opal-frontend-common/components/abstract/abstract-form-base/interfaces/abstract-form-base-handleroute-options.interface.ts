9e7b3c3afd3ee8f0813d76a6cd5b07bcf8f4f932
louisbriggs <louis.briggs@hmcts.net>
2025-12-01 09:42:00 +0000

    PO 1127 (#303)
    Co-authored-by: Frankie Moran <frankie.jack.moran@gmail.com>
    Co-authored-by: Arnab subedi <147511052+Arnabsubedi233@users.noreply.github.com>

---
+++ projects/opal-frontend-common/components/abstract/abstract-form-base/interfaces/abstract-form-base-handleroute-options.interface.ts
@@ -0,0 +1,7 @@
+export interface IAbstractFormBaseHandleRouteOptions {
+  nonRelative?: boolean;
+  event?: Event;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  routeData?: any;
+  fragment?: string;
+}
