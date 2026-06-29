4cf5fbda951015405ae25716a8ea5fe29068e264
Christopher Garratt <chris.garratt@cgi.com>
2026-06-29 12:13:15 +0100

    PO-8300 Adding allowedLengths parameter with default to phone number validator (#564)

--- projects/opal-frontend-common/validators/optional-valid-telephone/optional-valid-telephone.validator.ts
+++ projects/opal-frontend-common/validators/optional-valid-telephone/optional-valid-telephone.validator.ts
@@ -1,12 +1,12 @@
 import { ValidatorFn, AbstractControl } from '@angular/forms';
 
-export function optionalPhoneNumberValidator(): ValidatorFn {
+export function optionalPhoneNumberValidator(allowedLengths: readonly number[] = [11]): ValidatorFn {
   const numericPattern = /^[\d\s]*$/;
   return (control: AbstractControl): { [key: string]: unknown } | null => {
     if (control.value) {
       const valueWithoutSpaces = control.value.replaceAll(/\s+/g, '');
       const isValidPattern = numericPattern.test(control.value);
-      const isValidLength = valueWithoutSpaces.length === 11;
+      const isValidLength = allowedLengths.includes(valueWithoutSpaces.length);
       const valid = isValidPattern && isValidLength;
       return valid ? null : { phoneNumberPattern: { value: control.value } };
     }
