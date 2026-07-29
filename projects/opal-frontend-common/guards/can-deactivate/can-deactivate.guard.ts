88b7de29fc37bbd9e9a2f60c9e937c6d8fb55220
Christopher Garratt <chris.garratt@cgi.com>
2026-07-29 13:09:26 +0100

    Adding information loss guard

--- projects/opal-frontend-common/guards/can-deactivate/can-deactivate.guard.ts
+++ projects/opal-frontend-common/guards/can-deactivate/can-deactivate.guard.ts
@@ -1,6 +1,14 @@
 import { CanDeactivateFn } from '@angular/router';
 import { ICanDeactivateCanComponentDeactivate } from './interfaces/can-deactivate-can-component-deactivate.interface';
 
+export const canDeactivateInformationLossGuard: CanDeactivateFn<ICanDeactivateCanComponentDeactivate> = (
+  component: ICanDeactivateCanComponentDeactivate,
+) => {
+  return component.canDeactivate()
+    ? true
+    : confirm('WARNING: Are you sure you want to cancel? Any information you entered will be lost.');
+};
+
 export const canDeactivateGuard: CanDeactivateFn<ICanDeactivateCanComponentDeactivate> = (
   component: ICanDeactivateCanComponentDeactivate,
 ) => {
