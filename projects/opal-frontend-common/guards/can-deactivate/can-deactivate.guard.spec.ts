e01614901b3d86ab644dcab7dbf5812376c57962
Christopher Garratt <chris.garratt@cgi.com>
2026-08-07 12:02:27 +0100

    Updating wording on existing cancel guard

--- projects/opal-frontend-common/guards/can-deactivate/can-deactivate.guard.spec.ts
+++ projects/opal-frontend-common/guards/can-deactivate/can-deactivate.guard.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { ICanDeactivateCanComponentDeactivate } from './interfaces/can-deactivate-can-component-deactivate.interface';
-import { canDeactivateGuard, canDeactivateInformationLossGuard } from './can-deactivate.guard';
+import { canDeactivateGuard } from './can-deactivate.guard';
 import { describe, beforeEach, it, vi, expect } from 'vitest';
 
 describe('canDeactivateGuard', () => {
@@ -11,7 +11,6 @@ describe('canDeactivateGuard', () => {
   let mockNextState: RouterStateSnapshot;
 
   beforeEach(() => {
-    vi.restoreAllMocks();
     TestBed.configureTestingModule({});
 
     mockCurrentRoute = {} as ActivatedRouteSnapshot;
@@ -42,7 +41,7 @@ describe('canDeactivateGuard', () => {
     expect(result).toBe(false);
     expect(mockComponent.canDeactivate).toHaveBeenCalled();
     expect(window.confirm).toHaveBeenCalledWith(
-      'WARNING: You have unsaved changes. Press Cancel to go back and save these changes, or OK to lose these changes.',
+      'WARNING: Are you sure you want to cancel? Any information you entered will be lost.',
     );
   });
 
@@ -58,53 +57,7 @@ describe('canDeactivateGuard', () => {
     expect(result).toBe(true);
     expect(mockComponent.canDeactivate).toHaveBeenCalled();
     expect(window.confirm).toHaveBeenCalledWith(
-      'WARNING: You have unsaved changes. Press Cancel to go back and save these changes, or OK to lose these changes.',
-    );
-  });
-
-  describe('canDeactivateInformationLossGuard', () => {
-    it('should return true without displaying a confirmation if the component can deactivate', () => {
-      mockComponent = {
-        canDeactivate: vi.fn().mockReturnValue(true),
-      };
-      const confirmSpy = vi.spyOn(globalThis, 'confirm');
-
-      const result = canDeactivateInformationLossGuard(
-        mockComponent,
-        mockCurrentRoute,
-        mockCurrentState,
-        mockNextState,
-      );
-
-      expect(result).toBe(true);
-      expect(mockComponent.canDeactivate).toHaveBeenCalled();
-      expect(confirmSpy).not.toHaveBeenCalled();
-    });
-
-    it.each([
-      { confirmationResult: false, expectedResult: false },
-      { confirmationResult: true, expectedResult: true },
-    ])(
-      'should return $expectedResult when the confirmation returns $confirmationResult',
-      ({ confirmationResult, expectedResult }) => {
-        mockComponent = {
-          canDeactivate: vi.fn().mockReturnValue(false),
-        };
-        vi.spyOn(globalThis, 'confirm').mockReturnValue(confirmationResult);
-
-        const result = canDeactivateInformationLossGuard(
-          mockComponent,
-          mockCurrentRoute,
-          mockCurrentState,
-          mockNextState,
-        );
-
-        expect(result).toBe(expectedResult);
-        expect(mockComponent.canDeactivate).toHaveBeenCalled();
-        expect(window.confirm).toHaveBeenCalledWith(
-          'WARNING: Are you sure you want to cancel? Any information you entered will be lost.',
-        );
-      },
+      'WARNING: Are you sure you want to cancel? Any information you entered will be lost.',
     );
   });
 });
--- projects/opal-frontend-common/guards/can-deactivate/can-deactivate.guard.ts
+++ projects/opal-frontend-common/guards/can-deactivate/can-deactivate.guard.ts
@@ -1,20 +1,10 @@
 import { CanDeactivateFn } from '@angular/router';
 import { ICanDeactivateCanComponentDeactivate } from './interfaces/can-deactivate-can-component-deactivate.interface';
 
-export const canDeactivateInformationLossGuard: CanDeactivateFn<ICanDeactivateCanComponentDeactivate> = (
-  component: ICanDeactivateCanComponentDeactivate,
-) => {
-  return component.canDeactivate()
-    ? true
-    : confirm('WARNING: Are you sure you want to cancel? Any information you entered will be lost.');
-};
-
 export const canDeactivateGuard: CanDeactivateFn<ICanDeactivateCanComponentDeactivate> = (
   component: ICanDeactivateCanComponentDeactivate,
 ) => {
   return component.canDeactivate()
     ? true
-    : confirm(
-        'WARNING: You have unsaved changes. Press Cancel to go back and save these changes, or OK to lose these changes.',
-      );
+    : confirm('WARNING: Are you sure you want to cancel? Any information you entered will be lost.');
 };
