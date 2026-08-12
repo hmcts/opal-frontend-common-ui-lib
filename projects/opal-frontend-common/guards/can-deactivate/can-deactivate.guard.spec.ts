046db76a79dfb8cf9c9e8e647a1645051a1f6da3
Christopher Garratt <chris.garratt@cgi.com>
2026-08-12 17:13:16 +0100

    PO-9133 Further update to wording on cancel guard warning (#648)

--- projects/opal-frontend-common/guards/can-deactivate/can-deactivate.guard.spec.ts
+++ projects/opal-frontend-common/guards/can-deactivate/can-deactivate.guard.spec.ts
@@ -41,7 +41,7 @@ describe('canDeactivateGuard', () => {
     expect(result).toBe(false);
     expect(mockComponent.canDeactivate).toHaveBeenCalled();
     expect(window.confirm).toHaveBeenCalledWith(
-      'WARNING: Are you sure you want to cancel? Any information you entered will be lost.',
+      'WARNING: Are you sure you want to leave this page? Any information you entered will be lost.',
     );
   });
 
@@ -57,7 +57,7 @@ describe('canDeactivateGuard', () => {
     expect(result).toBe(true);
     expect(mockComponent.canDeactivate).toHaveBeenCalled();
     expect(window.confirm).toHaveBeenCalledWith(
-      'WARNING: Are you sure you want to cancel? Any information you entered will be lost.',
+      'WARNING: Are you sure you want to leave this page? Any information you entered will be lost.',
     );
   });
 });
